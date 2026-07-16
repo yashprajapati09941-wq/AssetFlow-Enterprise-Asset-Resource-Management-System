ffdafeab699fd54ca122bf7f181d66afe38eccd0
Yash Prajapati <yashprajapati09941@gmail.com>
2026-07-16 21:41:37 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// AssetFlow JavaScript
+
+console.log("AssetFlow Loaded Successfully");
+
+// Dashboard Data
+const totalAssets = 25;
+const availableAssets = 18;
+const allocatedAssets = 7;
+const totalEmployees = 12;
+
+// Show welcome message when page loads
+window.onload = function () {
+    alert("Welcome to AssetFlow Dashboard");
+};
